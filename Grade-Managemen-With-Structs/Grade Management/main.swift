fb95993786c976ae303ef59607d43c55bd4854fb
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2024-02-15 10:22:17 -0800

    Add files via upload

--- Grade-Managemen-With-Structs/Grade Management/main.swift
+++ Grade-Managemen-With-Structs/Grade Management/main.swift
@@ -5,23 +5,26 @@
 //  Created by StudentAM on 1/29/24.
 //
 
+//import Foundation, and CSV to be able to read file
 import Foundation
 import CSV
 
-struct person 
+//makes struct to be able to store student name, student grades, and final grades
+struct person
 {
     var StudentName: String
     var studentGrades: [Int]
     var studentFinalGrade: Int
 }
+//makes an array to store all names of students and structs.
 var students: [person] = []
-//var student: person = person(StudentName: <#T##String#>, studentGrades: [1,2,3,4,5,6], studentFinalGrade: <#T##Int#>)
 
+//start screen reads the CSV file, stores needed variables, and ask what the user wants to do
 func startScreen()
 {
     do {
-        
-        let stream = InputStream(fileAtPath: "/Users/studentam/desktop/Grade-Management/Grades.csv")
+        //finds the file at listed directory
+        let stream = InputStream(fileAtPath: "/Users/studentam/desktop/Grade-Managemen-With-Structs/Grades.csv")
         
         guard let csvStream = stream else {
             print("Failed to open CSV file.")
@@ -30,8 +33,9 @@ func startScreen()
         
         let csv = try CSVReader(stream: csvStream)
         
+        //assigns the student names , and grades to the variables in the struct.
         while let row = csv.next() {
-            var studentNames = (row[0])
+            let studentNames = (row[0])
             var studentGrades: [Int] = []
             for i in 1..<row.count {
                 if let grade1 = Int(row[i]) {
@@ -48,7 +52,7 @@ func startScreen()
     } catch {
         print("Error reading CSV file: \(error)")
     }
-    
+    //ask the user what they want to do and then calls requsted function.
     print ("Welcome, what would you like to do")
     print ("1. Display grade of a single student")
     print ("2. Display all grades for a student")
@@ -107,6 +111,7 @@ func startScreen()
     }
 }
 
+//function will take all students grades entered into the parameter then it will add them and divide by the number of grades to get average.
 func calcGrade(studentGrades: [Int]) -> Double
 {
     var current2: Int = 0
@@ -115,19 +120,19 @@ func calcGrade(studentGrades: [Int]) -> Double
     {
         current2 += j
     }
-    var average = Double(current2) / Double(studentGrades.count)
+    let average = Double(current2) / Double(studentGrades.count)
     return (average)
 }
-
+// after entering students name if it is found then it will print thier grade
 func singleStudentsGrade() {
     print("Enter the name of the student:")
     if let inputName = readLine() {
         for i in students.indices
         {
-            if inputName.lowercased() == students[i].StudentName
+            if inputName.lowercased() == students[i].StudentName.lowercased()
             {
                 print("\(inputName) found")
-                print("\(inputName)'s grade is: \(calcGrade(studentGrades: [])[i])")
+                print("\(inputName)'s grade is: \(students[i].studentFinalGrade)")
             }
         }
         sleep(2)
@@ -135,18 +140,18 @@ func singleStudentsGrade() {
     }
 }
 
-
+// if students name is entered and it exist then it will display the grades
 func singleStudentsGrades()
 {
     
     print("Enter the name of the student:")
     if let inputName = readLine() {
-        for i in studentNames.indices
+        for i in students.indices
         {
-            if inputName == studentNames[i]
+            if inputName.lowercased() == students[i].StudentName.lowercased()
             {
                 print("\(inputName) found")
-                print("name:\(studentNames[i]) Grades:\(grade[i])")
+                print("name:\(inputName) Grades:\(students[i].studentGrades)")
                 sleep(2)
                 startScreen()
             }
@@ -157,71 +162,98 @@ func singleStudentsGrades()
     }
 }
 
+// this uses a for in loop to display all of the names of the students and the grades
 func allStudentsGrades()
 {
     print("here are all the grades:")
     sleep(1)
-    for i in studentNames.indices
+    for i in students.indices
     {
-        print("Name:\(studentNames[i]) Grades: \(grade[i])")
+        print("Name:\(students[i].StudentName) Grades: \(students[i].studentGrades)")
     }
     sleep(2)
     startScreen()
 }
 
+// this will take all of the final grades and then get the average of all of them
 func averageOfClass()
 {
     var num = 0
-    for i in grade.indices
+    for i in students.indices
     {
-        num += calcGrade()[i]
+        num += Int(calcGrade(studentGrades: students[i].studentGrades))
     }
-    num /= grade.count
+    num /= students.count
     print("The average of the class is \(num)")
     sleep(1)
     num = 0
     startScreen()
 }
 
+// the average on one assignment takes the index of the selected assignment and then adds all of them together and divides by the ammount on that assignment.
 func averageOnAssignment()
 {
     var current2 = 0
     print("what assignment would you like to see the average of (1-10)?")
     if let input1 = Int(readLine()!)
     {
-        for i in grade.indices
+        if input1 > 10 || input1 < 1
         {
-            current2 += grade[i][input1-1]
+            print("please enter a valid number")
+            averageOnAssignment()
         }
-        current2 /= studentNames.count
+        for i in students.indices
+        {
+            current2 += students[i].studentGrades[input1-1]
+        }
+        current2 /= students.count
         print("the average of that assignement is: \(current2)")
         current2 = 0
         sleep(2)
         startScreen()
     }
 }
-
+// lowest grade will make the first student have the current lowest and then it will compare all of the students after that and see if it is lower.
 func lowestGrade()
 {
-    if let lowest = calcGrade().min(), let index = calcGrade().firstIndex(of: lowest)
-    {
-        print ("the lowest grade is by: \(studentNames[index]) with a grade of: \(lowest)")
+    
+    //assume the final grade of the first studend is the lowest
+    var lowest = students[0].studentFinalGrade
+    var name: String = ""
+    
+    for student in students{
+        if student.studentFinalGrade < lowest
+        {
+            lowest = student.studentFinalGrade
+            name = student.StudentName
+        }
     }
+    
+    print("The lowest grade is by: \(name) with a grade of: \(lowest)")
     sleep(1)
     startScreen()
-    //    if let minNumber = numbers.min(), let index = numbers.firstIndex(of: minNumber) {
 }
 
+// Highest grade will make the first student have the current highest and then it will compare all of the students after that and see if it is higher.
 func highestGrade()
 {
-    if let highest = calcGrade().max(), let index = calcGrade().firstIndex(of: highest)
-    {
-        print ("the lowest grade is by: \(studentNames[index]) with a grade of: \(highest)")
+    //assume the final grade of the first studend is the lowest
+    var highest = students[0].studentFinalGrade
+    var name: String = ""
+    
+    for student in students{
+        if student.studentFinalGrade > highest
+        {
+            highest = student.studentFinalGrade
+            name = student.StudentName
+        }
     }
+    
+    print("The lowest grade is by: \(name) with a grade of: \(highest)")
     sleep(1)
-    startScreen()
-}
+    startScreen()}
 
+//when filtering out the grades it takes the input for the low end and high end then it will print all of the grades between the wanted range.
 func filter() {
     print("Enter the low range you would like to use?")
     var lowRange = 0
@@ -276,17 +308,17 @@ func filter() {
         filter()
     }
 
-    for i in studentNames.indices
+    for i in students.indices
     {
-        let averageGrade = calcGrade()[i]
-        if averageGrade > lowRange && averageGrade < highRange 
+        let averageGrade = calcGrade(studentGrades: students[i].studentGrades);
+        if Int(averageGrade) > lowRange && Int(averageGrade) < highRange
         {
-            print("\(studentNames[i]): \(averageGrade)")
+            print("\(students[i].StudentName): \(averageGrade)")
         }
     }
     sleep(2)
     startScreen()
 }
 
-
+// put function call at the end then so it can delcare all variables then launch.
 startScreen()
